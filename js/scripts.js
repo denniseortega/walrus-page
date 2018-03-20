abde9da9a959961940c2b6f2c7d35c27204379de
Dennise Ortega and Eva Antipina <eva@>
2018-03-20 11:49:42 -0700

    add fadeToggle, slideDown, slideUp, and slideToggle

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,24 @@ $(document).ready(function(){
     $("#image-showing").toggle();
     $("#image-hidden").toggle();
   });
+
   $("#button1").click(function() {
     $("#info").fadeOut("slow");
   });
   $("#button2").click(function() {
     $("#info").fadeIn(1500);
   });
+  $(".click").click(function(){
+    $("#image-nofade").fadeToggle("slow", "swing", "callback");
+  });
+  $("#buttonUp").click(function(){
+    $("#sliding-info").slideUp();
+  });
+  $("#buttonDown").click(function() {
+    $("#sliding-info").slideDown();
+  });
+  $("#buttonSlideToggle").click(function() {
+    $("#image-nofade").slideToggle();
+  });
+
 });
